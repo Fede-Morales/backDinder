2dc396c3d55231c05a122b894010d716c9595785
Fede-Morales <danifmoraless@gmail.com>
2022-11-27 22:09:12 -0300

    creacionProyeto

---
+++ src/main/java/com/example/dinder/DinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.dinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dinder/DinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
